81ac235cca95a7634efaa5de388706705f21bc0b
MarneeDear <>
2019-10-05 14:00:33 -0700

    init s3 example

---
+++ src/console/Program.fs
@@ -0,0 +1,21 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Serilog
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    let logger = new LoggerConfiguration()
+    logger.WriteTo.Console() |> ignore //writing to console works
+
+    logger.WriteTo.File("D:\\myapp.log", rollingInterval = RollingInterval.Minute) |> ignore //writing to file works
+
+    //writing to S3 does not work
+    //No errors are produced
+    //No request is see on the buket
+    logger.WriteTo.AmazonS3("log.txt", "mister-logging", Amazon.RegionEndpoint.USWest2, "REDACTED", "REDACTED/REDACTED") |> ignore
+
+    Log.Logger <- logger.CreateLogger()
+    Log.Information("Hello, Serilog")
+    0 // return an integer exit code
